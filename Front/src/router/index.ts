29e30145353c4bccb77a2f9fef318b7ab07435c7
심대수 <tlaeotn123@naver.com>
2019-01-22 13:32:24 +0900

    Front vue,ts,webpack 초기 작업.

---
+++ Front/src/router/index.ts
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// 뷰 어플리케이션에 라우터 플러그인을 추가한다.
+// TODO: 추후 code splitting 을 위해서 동적 routing 하도록 하자.
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        { path: '/', component: { template: '<div> Home Page </div>' } },
+    ]
+})
+
+export default router
